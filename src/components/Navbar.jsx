fa1797918c698d7883dedb53ad342555bfb827cd
Harman Deep Singh <harman88157@gmail.com>
2026-01-30 12:50:43 +0530

    Small update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -76,11 +76,11 @@ const Navbar = () => {
         </nav>
 
         {/* Right Button */}
-        <div>
+        {/* <div>
           <button className="bg-blue-600 px-4 py-2 rounded hover:bg-blue-700 transition">
             Resume
           </button>
-        </div>
+        </div> */}
       </div>
     </header>
   );
